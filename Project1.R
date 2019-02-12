219641f47aaf8bff95f7dffa0f14e0f8b9ed0252
Timothy Lytkine <tlytkine@gmail.com>
2019-02-11 21:03:20 -0500

    Made nice graph lol

--- Project1.R
+++ Project1.R
@@ -7,4 +7,16 @@ library(igraph)
 edges1 <- read.table("roadNet-CA.txt")
 em <-as.matrix(edges1)
 
-  
+rows <- em[1:nrow(em),]
+rows
+
+v1 <- rows[0:1000,1]
+v2 <- rows[0:1000,2]
+v1
+v2
+
+relations <- data.frame(from=v1,to=v2)  
+
+
+g <- graph.data.frame(relations,directed=TRUE)
+plot(g)
